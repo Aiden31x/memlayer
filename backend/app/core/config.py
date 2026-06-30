c15c3b49136d27f2298a1f841e2fd16378f4fbd0
Aiden31x <aryandadwal2004@gmail.com>
2026-06-30 21:32:52 +0530

    added embeddings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,9 +4,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     google_api_key: str
     model_name: str = 'gemini-3-flash-preview'
+    embedding_model: str = 'all-MiniLM-L6-v2'
     database_url: str
 
     model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+
+
 
 
 settings = Settings()
---
+++ backend/app/services/embeddings.py
@@ -0,0 +1,11 @@
+from backend.app.core.config import settings
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer(settings.embedding_model)
+
+class EmbeddingsService:
+
+    @staticmethod
+    def embed_text(text: str) -> list[float]:
+        embedding = model.encode(text,convert_to_numpy=True,normalize_embeddings=True)
+        return embedding.tolist()
